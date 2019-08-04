bce036de347517323b2316ecf6254c4ee39594c4
Rodrigo Bedutti da Silva <rodrigobedutti@gmail.com>
2019-08-04 13:08:09 -0300

    Termino do exercício 6
    Termino do exercício sem o auxilio do vídeo.

---
+++ Aula 16/function01.js
@@ -0,0 +1,5 @@
+function ehImpar(n){
+    return n%2
+}
+const num = 356548666;
+console.log(`O número ${num} é ${ehImpar(num) ? "impar" : "par"}!`)
---
+++ Aula 16/function04.js
@@ -0,0 +1,13 @@
+function fatorial(n){
+    if(n == 1){
+        return 1
+    }else if(n < 1){
+        return 0
+    }else {
+        return n * fatorial(n-1)
+    }
+}
+console.log(fatorial(105))
+//5! = 5 x 4 x 3 x 2 x 1
+//5! = 5 x 4!
+//n! = n x (n-1)!
